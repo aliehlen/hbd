72088af25bfe0a22b0f55e5a971fb413b5ff09ea
aliehlen <ali.ehlen@gmail.com>
2021-01-31 15:33:48 -0600

    make suggested changes

--- src/main_code.py
+++ src/main_code.py
@@ -10,3 +10,9 @@ def other_cool_function():
 
 if __name__ == "__main__":
     print("hi, dheepak! this commit isn't really important")
+
+    print("print statements are very useful for debugging")
+
+    print("hint: the answer is related to how long we've known each other")
+
+    print("this isn't the longest puzzle, but maybe it's a start")
